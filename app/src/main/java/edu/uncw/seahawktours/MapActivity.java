cbd11c0443aa09ee785251d5ab1b65f69f6c89d2
acb4905 <37702798+acb4905@users.noreply.github.com>
2018-12-04 13:49:48 -0500

    -Map works when getting users location -Need to create the nearby building list now and work on the add item code for the ListView -Create a message in the building list section when the users location is not being used -Set up geofences and update nearby list when within a geofence

--- app/src/main/java/edu/uncw/seahawktours/MapActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MapActivity.java
@@ -21,7 +21,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 import android.widget.ListView;
+
+import com.google.android.gms.common.internal.Constants;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.GeoDataClient;
